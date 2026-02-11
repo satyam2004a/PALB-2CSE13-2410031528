d98d5dade720416b1921f251821692892cf706da
satyam2004a <satyam.raj.cs28@iilm.edu>
2026-02-11 12:41:02 +0530

    Add files via upload
    ass no 1,2,3

---
+++ main.py
@@ -0,0 +1,35 @@
+arr = [1, 2, 3, 4, 5]
+arr.reverse()
+print(arr)
+
+
+arr = [3, 5, 1, 9, 2]
+
+minimum = min(arr)
+maximum = max(arr)
+
+print("Minimum element:", minimum)
+print("Maximum element:", maximum)
+
+arr = [7, 10, 4, 3, 20, 15]
+k = 3
+
+arr.sort()
+print("Kth smallest element:", arr[k-1])
+
+
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 6, 7]
+
+union = list(set(a) | set(b))
+print(union)
+
+
+arr = [10, 20, 4, 45, 99]
+
+largest = max(arr)
+print("Largest element:", largest)
+
+
+
+
