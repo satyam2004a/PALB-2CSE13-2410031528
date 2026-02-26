8e48b0e3efdf2e3fc9d9405ad187f3b479bcfdce
satyam2004a <satyam.raj.cs28@iilm.edu>
2026-02-26 15:12:03 +0530

    Add files via upload

---
+++ exp 16.py
@@ -0,0 +1,13 @@
+def find_median(arr):
+    arr.sort()
+    n = len(arr)
+
+    if n % 2 == 1:
+        return arr[n // 2]
+    else:
+        return (arr[n // 2 - 1] + arr[n // 2]) / 2
+
+
+print(find_median([90, 100, 78, 89, 67]))
+print(find_median([56, 67, 30, 79]))
+print(find_median([1, 2]))
