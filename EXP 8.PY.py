e2596a019e72c227b0c26e65fc9d075faeb9ce24
satyam2004a <satyam.raj.cs28@iilm.edu>
2026-02-26 15:08:39 +0530

    Add files via upload

---
+++ EXP 8.PY.py
@@ -0,0 +1,18 @@
+nums = [1, 3, 5, 6]
+target = 5
+
+left = 0
+right = len(nums) - 1
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if nums[mid] == target:
+        print("Index =", mid)
+        break
+    elif nums[mid] < target:
+        left = mid + 1
+    else:
+        right = mid - 1
+else:
+    print("Index =", left)
