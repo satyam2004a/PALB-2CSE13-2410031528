8e9b79fa8ce91a3975f0985cff209b3c0f4ada04
satyam2004a <satyam.raj.cs28@iilm.edu>
2026-02-26 15:06:13 +0530

    Add files via upload

---
+++ EXP 2.PY.py
@@ -0,0 +1,7 @@
+#Given an array arr[]. Your task is to find the minimum and maximum elements in the array.
+
+arr = [1, 4, 3, 5, 8, 6]
+res = []
+res.append(min(arr))
+res.append(max(arr))
+print(res)
